cc4a371aa446551f8c689c3609014017ab35c75d
miokawai <miokawai1210@gmail.com>
2015-10-07 19:35:34 +0000

    add followings/followers

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,13 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
+        if params[:asin]
         response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin],
+                          :response_group =>'Medium',
+                          :country => 'jp')
+        @amazon_items = response.items
+      end
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -30,7 +36,11 @@ class OwnershipsController < ApplicationController
     # params[:type]の値ににHaveボタンが押された時には「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
     
-
+    if params[:type] == "Have"
+      current_user.have(@item)
+    elsif  params[:type] == "Want"
+    current_user.want(@item)
+  end
   end
 
   def destroy
@@ -38,5 +48,10 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値ににHavedボタンが押された時には「Have」,
     # Wantedボタンがされた時には「Want」が設定されています。
+    if params[:type] == "Have"
+      current_user.unhave(@item)
+    elsif params[:type] == "Want"
+    current_user.unwant(@item)
+    end
   end
 end
